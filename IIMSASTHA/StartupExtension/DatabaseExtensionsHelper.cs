1dc8465d783af368ccbcd96482598c8046c154cf
Md Faizul Haque <csldotnet@gmail.com>
2026-03-18 19:31:15 +0600

    Project Updated

---
+++ IIMSASTHA/StartupExtension/DatabaseExtensionsHelper.cs
@@ -0,0 +1,32 @@
+﻿using IIMSASTHA.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace IIMSASTHA.StartupExtension
+{
+    public static class DatabaseExtensionsHelper
+    {
+        public static IServiceCollection AddDatabaseExtensionHelper(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(Opt =>
+            {
+                Opt.UseSqlServer(configuration.GetConnectionString(name: "DefaultConnection"));
+
+            });
+
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
+            return services;
+        }
+
+        public static IApplicationBuilder RunMigration(this IApplicationBuilder app)
+        {
+            using (var scope = app.ApplicationServices.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+            return app;
+        }
+    }
+}

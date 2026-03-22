3bf1383c13b81bf508b790f027786623828eff8d
Md Faizul Haque <csldotnet@gmail.com>
2026-03-22 22:27:26 +0600

    Class Created

---
+++ IIMSASTHA/Models/Vascard.cs
@@ -0,0 +1,15 @@
+﻿namespace IIMSASTHA.Models
+{
+    public class Vascard
+    {
+        public int EmpID { get; set; }
+        public int Code { get; set; }
+        public string Image { get; set; }
+        public string SigImage { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+        public string Blood { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Status { get; set; }
+    }
+}

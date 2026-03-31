9ddb98ff793c410168ed5176562faee04cb33fbc
Md Faizul Haque <csldotnet@gmail.com>
2026-03-31 13:49:50 +0600

    Create Operation Completed

--- IIMSASTHA/Controllers/VascardController.cs
+++ IIMSASTHA/Controllers/VascardController.cs
@@ -2,6 +2,7 @@
 using IIMSASTHA.Interfaces;
 using IIMSASTHA.Models;
 using Microsoft.AspNetCore.Mvc;
+using IHostingEnvironment = Microsoft.AspNetCore.Hosting.IHostingEnvironment;
 
 namespace IIMSASTHA.Controllers
 {
@@ -11,10 +12,13 @@ namespace IIMSASTHA.Controllers
 
         private readonly Ivascard _ivascard;
 
-        public VascardController(ApplicationDbContext ctx, Ivascard ivascard)
+        private readonly IWebHostEnvironment _env; 
+
+        public VascardController(ApplicationDbContext ctx, Ivascard ivascard, IWebHostEnvironment env)
         {
            _context = ctx;
            _ivascard = ivascard;
+           _env = env;
         }
 
         public IActionResult Index()
@@ -22,14 +26,53 @@ namespace IIMSASTHA.Controllers
             List<Vascard> vcrd = _ivascard.GetAllVascard();
             return View(vcrd);
         }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
         [HttpPost]
-        public IActionResult Create(Vascard vcrd)
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Create(Vascard vcrd,IFormFile ImageFile,IFormFile SignatureFile)
         {
+            if (ImageFile != null)
+            {
+                
+                string fileName = Path.GetFileName(ImageFile.FileName);
+                string filePath = Path.Combine("wwwroot/Vascard", fileName);
+                
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+
+                vcrd.ImageUrl = "/Vascard/" + fileName;     
+            }
+            else
+            {
+                vcrd.ImageUrl = "default.jpg"; 
+            }
+
+            if (SignatureFile != null)
+            {
+
+                string fileName = Path.GetFileName(SignatureFile.FileName);
+                string filePath = Path.Combine("wwwroot/Vascard", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await SignatureFile.CopyToAsync(stream);
+                }
+
+                vcrd.SigImageUrl = "/Vascard/" + fileName;  
+            }
+            else
+            {
+                vcrd.SigImageUrl = "default.jpg"; 
+            }
             try
             {
                 _ivascard.AddVascard(vcrd);
@@ -42,36 +85,109 @@ namespace IIMSASTHA.Controllers
             }
             return RedirectToAction(actionName:nameof(Index));
         }
+
         [HttpGet]
         public IActionResult Edit(int id)
         {
             var vscrd = _ivascard.GetVascardById(id);
             return View(vscrd);
         }
+
         [HttpPost]
-        public IActionResult Edit(Vascard vcrd)
+        [ValidateAntiForgeryToken]
+        public async Task <IActionResult> Edit(Vascard vcrd,IFormFile ImageFile, IFormFile SignatureFile)
         {
-            _ivascard.UpdateVascard(vcrd);
-            _context.SaveChanges();
+
+            if (ImageFile != null)
+            {
+                string fileName = Path.GetFileName(ImageFile.FileName);
+                string filePath = Path.Combine("wwwroot/Vascard", fileName);
+
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(stream);
+                }
+
+                vcrd.ImageUrl = "/Vascard/" + fileName;
+
+            }
+            else
+            {
+                vcrd.ImageUrl = "default.jpg";
+            }
+
+            if (SignatureFile != null)
+            {
+
+                string fileName = Path.GetFileName(SignatureFile.FileName);
+                string filePath = Path.Combine("wwwroot/Vascard", fileName);
+
+                using (var stream = new FileStream(filePath, FileMode.Create))
+                {
+                    await SignatureFile.CopyToAsync(stream);
+                }
+
+                vcrd.SigImageUrl = "/Vascard/" + fileName;
+            }
+            else
+            {
+                vcrd.SigImageUrl = "default.jpg";
+            }
+
+            try
+            {
+                _ivascard.UpdateVascard(vcrd);
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            
             return RedirectToAction(actionName: nameof(Index));
         }
+
         public IActionResult Details(int id)
         {
             var vscrd = _ivascard.GetVascardById(id);
             return View(vscrd);
         }
+
         [HttpGet]
         public IActionResult Delete(int id)
         {
             var vscrd = _context.vascards.Find(id);
             return View(vscrd);
         }
+
         [HttpPost, ActionName("Delete")]
         public IActionResult ConfirmDelete(int id)
         {
             _ivascard.DeleteVascard(id);
             _context.SaveChanges(true);
             return RedirectToAction(actionName: nameof(Index));
+        }
+
+        private string UplodedFile(Vascard vcrd) 
+        {
+            string uniqueFileName = null;
+
+            if (vcrd.ImageUrl !=null)
+            {
+
+                string uploadsFolder = Path.Combine(_env.WebRootPath,"images");
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + vcrd.ImageUrl;
+                uniqueFileName = Guid.NewGuid().ToString() + "_" + vcrd.SigImageUrl;
+                string filepath = Path.Combine(uploadsFolder, uniqueFileName);
+                using (var fileStream = new FileStream(filepath, FileMode.Create)) 
+                {
+                   
+                }
+            }
+            return uniqueFileName;
         }
     }
 }
--- IIMSASTHA/Models/Vascard.cs
+++ IIMSASTHA/Models/Vascard.cs
@@ -5,40 +5,42 @@ namespace IIMSASTHA.Models
     public class Vascard
     {
         [Key]
+        [Display(Name = "ID")]
         public int VascardId { get; set; }
 
         [Display(Name = "Code")]
-
         [Required]
         public int Code { get; set; }
-
+        
         [Display(Name = "Image")]
         [StringLength(500)]
-        [Required]
-        public string? ImageUrl{ get; set; }
+        [Required (ErrorMessage = " Please Choose Image")]
+        public string ImageUrl{ get; set; }
 
         [Display(Name = "Signature")]
         [StringLength(500)]
-        [Required]
-        public string? SigImageUrl { get; set; }
+        [Required (ErrorMessage = " Please Choose Signature")]
+        public string SigImageUrl { get; set; }
 
         [Display(Name = "Name")]
         [StringLength(100)]
         [Required]
-        public string? Name { get; set; }
+        public string Name { get; set; }
 
         [Display(Name = "Designation")]
         [StringLength(100)]
         [Required]
-        public string? Designation { get; set; }
+        public string Designation { get; set; }
 
         [Display(Name = "Blood Group")]
         [StringLength(100)]
         [Required]
-        public string? Blood { get; set; }
-        
+        public string Blood { get; set; }
+
+        [Display(Name = "Joining Date")]
         [Required]
         public DateTime JoiningDate { get; set; }
-        public string? Status { get; set; }
+
+        public string Status { get; set; }
     }
 }
